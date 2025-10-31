b953ad2ea0c8b4221657f4cbf6af285d28c424f5
Velociraptor115 <Velociraptor115@users.noreply.github.com>
2025-10-31 23:03:54 +0530

    [CommitOverview.vue] Add CommitOverview

---
+++ common-client/src/components/commit-overview/CommitOverview.vue
@@ -0,0 +1,132 @@
+<!--
+SPDX-FileCopyrightText: 2025 Velociraptor115
+
+SPDX-License-Identifier: LGPL-3.0-only
+-->
+
+<script setup lang="ts">
+import type { JJCommitGraphNodeData } from '@common/jj-graph-parser/commit-graph-parser';
+import NodeDescription from '../NodeDescription.vue';
+import { inject } from 'vue';
+import { GRAPH_ACTIONS_INJECTION_KEY } from '@common-client/providers/graph-actions-provider';
+import IdPrefix from '../IdPrefix.vue';
+
+const actions = inject(GRAPH_ACTIONS_INJECTION_KEY)!
+
+type NodeId = number
+
+type ThisComponentProps = {
+  nodeData: JJCommitGraphNodeData
+  color: string
+  highlighted: boolean
+  selected: boolean
+}
+
+type ThisComponentEmits = {
+  (e: 'mouseenter'): void
+  (e: 'mouseleave'): void
+  (e: 'click'): void
+}
+
+const { nodeData, color, selected, highlighted } = defineProps<ThisComponentProps>()
+const emit = defineEmits<ThisComponentEmits>()
+
+</script>
+
+<template>
+  <div class="display-contents graph-row" :class="{ 'selected': selected }" @mouseenter="emit('mouseenter')" @mouseleave="emit('mouseleave')" @click="emit('click')">
+    <div class="display-contents" v-if="nodeData.type === 'commit'">
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell">
+        <template v-if="highlighted || selected">
+          <!-- <span class="command" :class="{ 'disabled': nodeData.isImmutable }">D</span> -->
+          <button @click.stop="actions.edit(nodeData.changeId)"      class="command" :disabled="nodeData.isWorkingCopy || nodeData.isImmutable">E</button>
+          <button @click.stop="actions.describe(nodeData.changeId, nodeData.description)"   class="command" :disabled="nodeData.isImmutable"         >D</button>
+          <button @click.stop="actions.newAfter(nodeData.changeId)"  class="command"                                                           >A</button>
+          <button @click.stop="actions.newBefore(nodeData.changeId)" class="command" :disabled="nodeData.isImmutable"                          >B</button>
+          <button @click.stop="actions.abandon(nodeData.changeId)"   class="command" :disabled="nodeData.isImmutable || !nodeData.isEmpty || nodeData.parents.length > 1">X</button>
+        </template>
+      </div>
+      <div class="px-2 grid-cell node-description"><NodeDescription :node-data="nodeData" :color="color" /></div>
+      <div class="px-2 grid-cell"><pre>{{ nodeData.author.timestamp }}</pre></div>
+      <div class="px-2 grid-cell"><span>{{ nodeData.author.name }}</span></div>
+      <div class="px-2 grid-cell"><IdPrefix :id="nodeData.changeId" :prefix="nodeData.changeIdPrefixLen" :prefix-min="8" /></div>
+      <div class="px-2 grid-cell"><IdPrefix :id="nodeData.commitId" :prefix="nodeData.commitIdPrefixLen" :prefix-min="8" /></div>
+    </div>
+    <div class="display-contents" v-else-if="nodeData.type === 'elided'">
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell node-description"><NodeDescription :node-data="nodeData" :color="color" /></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+    </div>
+    <div class="display-contents" v-else-if="nodeData.type === 'commitId'">
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell node-description"><NodeDescription :node-data="nodeData" :color="color" /></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"><pre>{{ nodeData.commitId.slice(0, 8) }}</pre></div>
+    </div>
+    <div class="display-contents" v-else>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell node-description"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+      <div class="px-2 grid-cell"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.display-contents {
+  display: contents;
+}
+
+.graph-row {
+  cursor: pointer;
+}
+
+.graph-row:hover .grid-cell {
+  background-color: rgba(128, 128, 128, 0.15);
+}
+
+.graph-row.selected .grid-cell {
+  background-color: rgba(128, 128, 128, 0.25);
+}
+
+.graph-row.selected:hover .grid-cell {
+  background-color: rgba(128, 128, 128, 0.35);
+}
+
+.px-2 {
+  padding-inline: 0.5rem;
+}
+
+.node-description {
+  display: flex;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+
+  align-items: center;
+}
+
+.command {
+  padding-inline: 6px;
+}
+
+.command:not(.disabled):hover {
+  background-color: rgba(128, 128, 128, 0.5);
+}
+
+.command.disabled {
+  cursor: not-allowed;
+  color: rgb(from currentColor r g b / calc(alpha * 0.6));
+}
+</style>
